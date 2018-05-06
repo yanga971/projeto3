a4bb797ee1721acef0115edf167e2eaf316da3d0
yanga971 <yanga971@free.fr>
2018-05-06 23:45:23 +0200

    Ajout d'un marker

--- map.js
+++ map.js
@@ -1,14 +1,22 @@
-
 var map =  {
 
   // Initialisation de la carte
   initMap : function(){
-     var options = {
-       center: new google.maps.LatLng(45.764043,4.835659),
-       zoom: 11
-     };
-     var googleMap = new google.maps.Map(document.getElementById('googleMap'), options)
-  }
+              var options = {
+                center: new google.maps.LatLng(45.764043,4.835659),
+                zoom: 11
+              };
+              this.googleMap = new google.maps.Map(document.getElementById('googleMap'), options)
+            },
 
-}; //
+  // Ajout d'un marker
+  marker : function(){
+              new google.maps.Marker({
+                position:{lat:45.764043,lng:4.835659},
+                map:this.googleMap
+              });
+            }
+}; // Fin de l'objet map
+
 map.initMap();
+map.marker();
