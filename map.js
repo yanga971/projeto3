b9809e356043193e0ae628a4dd6494e113667250
yanga971 <yanga971@free.fr>
2018-05-07 08:45:28 +0200

    Ajout de markers pour l'ensemble des stations

--- map.js
+++ map.js
@@ -1,22 +1,33 @@
+var googleMap;
+// Objet map
 var map =  {
 
   // Initialisation de la carte
   initMap : function(){
-              var options = {
+                options = {
                 center: new google.maps.LatLng(45.764043,4.835659),
                 zoom: 11
               };
-              this.googleMap = new google.maps.Map(document.getElementById('googleMap'), options)
+                googleMap = new google.maps.Map(document.getElementById('googleMap'), options)
             },
 
-  // Ajout d'un marker
-  marker : function(){
-              new google.maps.Marker({
-                position:{lat:45.764043,lng:4.835659},
-                map:this.googleMap
+  // Ajout de markers pour chaque stations de vélos
+  markers : function(){
+              var markers = [];
+              ajaxGet('https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=c18bf95eec3f31e26c33a30a925adbdb7ed3cc55', function (stations){
+                stations = JSON.parse(stations);
+                stations.forEach(function(station) {
+                  // Création d'un marqueur pour chaque station
+                  var markers = new google.maps.Marker({
+                    position: station.position,
+                    map: googleMap
+                  });
+                });
               });
+              //Ajout du marqueur dans le tableau
+              markers.push("stations");
             }
 }; // Fin de l'objet map
 
 map.initMap();
-map.marker();
+map.markers();
